b563b996b7f4438824f1567cdb4b9bb367d9eda9
Eddy Parris <eddy@communecation.net>
2012-02-23 21:07:23 +0000

    Make the changes noted in pull request 2. https://github.com/nixmc/E-Advocacy-Social-Proof-Widget/pull/2
    * Test with JQuery 1.4
    * Create a template and debug CSS
    * Add comment about the JQuery dependency in the readme
    * Make url a required argument
    * Add some whitespace
    * Create minified version

--- jq-spw.js
+++ jq-spw.js
@@ -1,35 +1,47 @@
 // namespace our plugin
 (function($) {
 
-  $.fn.spwWidgetize = function(opts) {
+  $.fn.spwWidgetize = function(url, opts) {
     // start of plugin code
     var defaults = {
-      // fetch straight from Google docs in JSON
-      url: 'http://spreadsheets.google.com/feeds/list/0AgwxfJ2RdQrudDVKTl9aNHhVQlU1blFHcC1sZHhBOEE/2/public/basic?alt=json',
       auto_measurement_key: 'emailsSent',
       manual_measurement_key: 'manualMeasurement',
       target_key: 'target',
       measurement_text_key: 'measurementText',
       debug: false,
-      template_html: "\
-        <div id='measurement-text'><%= total %> <%= measurement_text %></div> \
-        <div id='progress-total'> \
-          <div id='progress-bar' style='width: <%= progress_width %>px;'></div> \
-        </div> \
-        <div id='stats'><span class='base-stat>0</span> <span class='target-stat><%= target %></span></div> \
-      ",
+      min_arrow: 3,
+      template_html: " \
+      <div id='widget-wrapper'> \
+				<h4><span class='widget-header-total'><%= total %></span> <span class='header-text'><%= measurement_text %></span></h4> \
+				<span id='widget-progress-arrow' style='left: <%= progress_arrow_position %>px;'></span> \
+				<span id='widget-progress-value' style='width: <%= progress_width %>px;'></span> \
+				<div id='widget-progress-meter'> \
+				</div> \
+				<h5><span class='widget-footer-base'>0</span><span class='widget-footer-target'><%= target %></span></h5> \
+			</div> \
+      "
     }
     // merge the opts with the defaults, overwriting the defaults
     opts = $.extend(true, defaults, opts);
-    
+    opts['url'] = url;
+
     $container = this;
     
     // Add the CSS to the head, this is for testing.
-    if(opts.debug){
+    if (opts.debug){
       $('head').append('\
         <style type="text/css"> \
           #progress-total{ width: 100%; height: 50px; background: #AAA;} \
           #progress-bar{ width: 0; height: 50px; background: #CCC;} \
+          #widget-wrapper { background: url("images/progress-bg.png") no-repeat scroll left top transparent; display: block; height: 130px; position: relative; top: 26px; width: 200px; color: #FFF; font-family: helvetica, ariel, sans-serif; } \
+          #widget-progress-arrow { background: url("images/progress-arrow.png") no-repeat scroll left top transparent; display: block; height: 11px; position: absolute; top: 75px; width: 21px; } \
+          #widget-progress-value { background: none repeat scroll 0 0 #EC008C; display: block; height: 10px; left: 0; position: absolute; top: 91px; z-index: 0; } \
+          #widget-progress-meter { background: url("images/progress-meter.png") no-repeat scroll left top transparent; height: 16px; left: 0; position: absolute; top: 89px; width: 200px; } \
+          #widget-wrapper h4 { padding: 10px; } \
+          #widget-wrapper h4 span.widget-header-total { font-size: 30px; text-align: center; display: block; clear: right;} \
+          #widget-wrapper h4 span.header-text { text-align: center; display: block;} \
+          #widget-wrapper h5 { position: relative; top: 10px; color: #000; font-size: 20px; } \
+          #widget-wrapper h5 span.widget-footer-target { float: right; } \
         </style> \
       ');
     }
@@ -41,10 +53,10 @@
         // sort out the data in to something useable
         var data = {};
         $(remote_data.feed.entry).each(function(idx, row) {
-          if(row.title.$t == opts.auto_measurement_key || row.title.$t == opts.manual_measurement_key || row.title.$t == opts.target_key || row.title.$t == opts.measurement_text_key){
+          if (row.title.$t == opts.auto_measurement_key || row.title.$t == opts.manual_measurement_key || row.title.$t == opts.target_key || row.title.$t == opts.measurement_text_key){
             data[row.title.$t] = row.content.$t.replace("value: ", "");
           }
-          if(opts.debug){
+          if (opts.debug){
             $("#"+row.title.$t).text(row.content.$t.replace("value: ", ""));
           }
         });
@@ -53,78 +65,80 @@
         data['total'] = parseInt(data[opts.auto_measurement_key]) + parseInt(data[opts.manual_measurement_key]);
         data['progress'] = parseInt(data['total']) / (parseInt(data[opts.target_key]) / 100);
         var progress_width = $container.width();
-        var progress_percent_in_px = (progress_width / 100) * data['progress'];
+        var progress_percent_in_px =  (progress_width / 100) * data['progress'];
+        var progress_arrow_position = progress_percent_in_px > opts.min_arrow + 10 ? parseInt(progress_percent_in_px - 11) : 3;
         
         // render the template
         template_html = template(opts.template_html, {
           total: data['total'],
           measurement_text: data[opts.measurement_text_key],
+          progress_arrow_position: progress_arrow_position,
           progress_width: progress_percent_in_px,
-          target: data[opts.target_key],
+          target: data[opts.target_key]
         });
         $container.html(template_html);
       }
     });
-  };
-  
-  /*
-   Templating from underscore.js
-   http://documentcloud.github.com/underscore/
-   Copyright (c) 2009-2012 Jeremy Ashkenas, DocumentCloud
+    
+    /*
+     Templating from underscore.js
+     http://documentcloud.github.com/underscore/
+     Copyright (c) 2009-2012 Jeremy Ashkenas, DocumentCloud
 
-   Permission is hereby granted, free of charge, to any person
-   obtaining a copy of this software and associated documentation
-   files (the "Software"), to deal in the Software without
-   restriction, including without limitation the rights to use,
-   copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following
-   conditions:
+     Permission is hereby granted, free of charge, to any person
+     obtaining a copy of this software and associated documentation
+     files (the "Software"), to deal in the Software without
+     restriction, including without limitation the rights to use,
+     copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the
+     Software is furnished to do so, subject to the following
+     conditions:
 
-   The above copyright notice and this permission notice shall be
-   included in all copies or substantial portions of the Software.
+     The above copyright notice and this permission notice shall be
+     included in all copies or substantial portions of the Software.
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-   OTHER DEALINGS IN THE SOFTWARE.
-  */
-  var noMatch = /.^/;
-  var unescape = function(code) {
-    return code.replace(/\\\\/g, '\\').replace(/\\'/g, "'");
-  };
-  
-  var template = function(str, data) {
-    var c  = {
-      evaluate    : /<%([\s\S]+?)%>/g,
-      interpolate : /<%=([\s\S]+?)%>/g,
-      escape      : /<%-([\s\S]+?)%>/g
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+     OTHER DEALINGS IN THE SOFTWARE.
+    */
+    var noMatch = /.^/;
+    var unescape = function(code) {
+      return code.replace(/\\\\/g, '\\').replace(/\\'/g, "'");
     };
-    var tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
-      'with(obj||{}){__p.push(\'' +
-      str.replace(/\\/g, '\\\\')
-         .replace(/'/g, "\\'")
-         .replace(c.escape || noMatch, function(match, code) {
-           return "',_.escape(" + unescape(code) + "),'";
-         })
-         .replace(c.interpolate || noMatch, function(match, code) {
-           return "'," + unescape(code) + ",'";
-         })
-         .replace(c.evaluate || noMatch, function(match, code) {
-           return "');" + unescape(code).replace(/[\r\n\t]/g, ' ') + ";__p.push('";
-         })
-         .replace(/\r/g, '\\r')
-         .replace(/\n/g, '\\n')
-         .replace(/\t/g, '\\t')
-         + "');}return __p.join('');";
-    var func = new Function('obj', '_', tmpl);
-    if (data) return func(data, function(obj) { return new wrapper(obj); });
-    return function(data) {
-      return func.call(this, data, function(obj) { return new wrapper(obj); });
+
+    var template = function(str, data) {
+      var c  = {
+        evaluate    : /<%([\s\S]+?)%>/g,
+        interpolate : /<%=([\s\S]+?)%>/g,
+        escape      : /<%-([\s\S]+?)%>/g
+      };
+      var tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
+        'with(obj||{}){__p.push(\'' +
+        str.replace(/\\/g, '\\\\')
+           .replace(/'/g, "\\'")
+           .replace(c.escape || noMatch, function(match, code) {
+             return "',_.escape(" + unescape(code) + "),'";
+           })
+           .replace(c.interpolate || noMatch, function(match, code) {
+             return "'," + unescape(code) + ",'";
+           })
+           .replace(c.evaluate || noMatch, function(match, code) {
+             return "');" + unescape(code).replace(/[\r\n\t]/g, ' ') + ";__p.push('";
+           })
+           .replace(/\r/g, '\\r')
+           .replace(/\n/g, '\\n')
+           .replace(/\t/g, '\\t')
+           + "');}return __p.join('');";
+      var func = new Function('obj', '_', tmpl);
+      if (data) return func(data, function(obj) { return new wrapper(obj); });
+      return function(data) {
+        return func.call(this, data, function(obj) { return new wrapper(obj); });
+      };
     };
   };
 })(jQuery);
